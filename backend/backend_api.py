2a81bc7746560b4ae15ac0d95eccd44ab497099f
Ryan Kovatch <39537451+ryank43@users.noreply.github.com>
2025-04-22 22:21:34 -0700

    Reformat file

--- backend/backend_api.py
+++ backend/backend_api.py
@@ -1,6 +1,6 @@
 from db_models import *
 from db_seeder import seed_db
-from flask import Flask, render_template, send_file, send_from_directory, request, jsonify, abort
+from flask import Flask, render_template, send_file, request, jsonify, abort
 from mongoengine import connect
 from os import environ as env
 import io
@@ -13,29 +13,35 @@ connect(host=f"mongodb://{env['MONGODB_HOSTNAME']}:27017/ara_db")
 if User.objects.count() == 0:
     seed_db()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Retrieves PDF from MongoDB database and sends to frontend to populate on app
-# http://localhost:5001/get_pdf?name=Sample_Survey_Highlights.pdf
+
 @app.route('/get_pdf', methods=['GET'])
 def get_pdf():
+    """
+    Retrieves a PDF from the MongoDB database and returns its bytes.
+    Ex: http://localhost:5001/get_pdf?name=dummy.pdf
+    """
     filename = request.args.get('name')
     pdf_file = PDF.objects(name=filename).first()
-    if pdf_file == None:
+    if pdf_file is None:
         abort(404)
     file_data = pdf_file.file.read()
     file_stream = io.BytesIO(file_data)
     return send_file(file_stream, mimetype='application/pdf', download_name=filename)
 
+
 @app.route('/get_notes', methods=['GET'])
 def get_notes():
     pass
 
-# Receives and uploads PDF to MongoDB database
+
 @app.route('/upload_pdf', methods=['POST'])
 def upload_pdf():
+    """Receives and uploads a PDF to the MongoDB database."""
     file = request.files['pdf_file']
     if file.filename.endswith(".pdf"):
         existing_pdf_check = PDF.objects(name=file.filename).first()
