f3a4ac580cff03d004e732008eeab7ac03ba46a1
claireccody <ccody_uo@hotmail.com>
2025-05-05 00:05:50 -0700

    adding comments :)

--- frontend/static/loadingTags.js
+++ frontend/static/loadingTags.js
@@ -1,14 +1,13 @@
+/** LOGIC AND FUNCTIONS ASSOCIATED WITH CHAPTERS & SECTIONS TAG COLUMN */
+
+//initializing On-Screen area and empty arrays for chapters and sections
 let chapArray = [];
-// let menu = document.getElementById("chapter");
-//let chap_id = null;
-// array for
 let sectTags = [];
 const menu = document.getElementById("chapter");
 
-//let tag_id = null;
 
 async function fetchChaps() {
-    // Fetches all existing PDFs from the database to populate PDFArray[]
+    // Fetches all existing Chapters from the PDF to populate chapArray[]
     return fetch(`http://localhost:5001/pdfs/${pdf_id}/chapters`, {
         method: 'GET'
     })
@@ -23,9 +22,8 @@ async function fetchChaps() {
       .then(data => {
         chapArray.length = 0;
         data.forEach(chap => {
-          chapArray.push({id: chap._id, title: chap.title, startPage: chap.start_page, /*notesCount: tag.notes_count */});
+          chapArray.push({id: chap._id, title: chap.title, startPage: chap.start_page});
         });
-        //displayDocOptions();
         console.log(chapArray)
         fillChapters();
       })
@@ -47,7 +45,7 @@ function setChap(){
   async function fetchTags() {
     console.log("entering fetchTags()", chap_id);
     clearSections();
-      // Fetches all existing PDFs from the database to populate PDFArray[]
+      // Fetches all existing sections from the chapter to populate sectTags[]
       return fetch(`http://localhost:5001/pdfs/${pdf_id}/chapters/${chap_id}/sections`, {
         method: 'GET'
     })
@@ -64,7 +62,6 @@ function setChap(){
         data.forEach(tag => {
           sectTags.push({title: tag.title, startPage: tag.start_page, notesCount: tag.notes_count, id: tag._id });
         });
-        //displayDocOptions();
         console.log(sectTags)
         updateSections();
       })
@@ -75,10 +72,9 @@ function setChap(){
       
   }
 
-  // loads chapters into select menu
-function fillChapters() {
-    console.log("helo");
 
+function fillChapters() {
+  // loads chapters into select menu
  for (let i = 0; i < chapArray.length; i++) {
     const chapHolder = document.createElement("div");
     chapHolder.className = "chap-obj";
@@ -93,21 +89,11 @@ function fillChapters() {
     menu.appendChild(chapHolder);
   }
 
-  // const noSelect = document.getElementById("no-select");
-  // noSelect.textContent = `Choose a Chapter`;
 }
 
 
-//   const chapBtn = document.getElementById("chapters-btn");
-//   chapBtn.innerHTML = `ADD NEW`;
-//   chapBtn.setAttribute("onclick", addChapter());
-
-//   const noSelect = document.getElementById("no-select");
-//   noSelect.textContent = `Choose a Chapter`;
-// }
-
-// displays sectTags as OSO (On Screen Object)
 function updateSections() {
+  // displays sectTags as OSO (On Screen Object)
     const tagList = document.getElementById("tags");
     tagList.innerHTML = "";
     for (let i = 0; i < sectTags.length; i++) {
@@ -146,8 +132,6 @@ function updateSections() {
   function changeSection(id){
     console.log("change", id);
     tag_id = id;
-    //getData();
-    //viewQuestions();
   }
 
   function clearSections() {
@@ -160,60 +144,3 @@ function updateSections() {
     quest.textContent = "";
   }
   
-//   function addSection() {
-//     const sectionName = document.getElementById("new-section").value.trim();
-//     //console.log(questionIn);
-//     const currChap = document.getElementById("chapter").value;
-  
-//     if (currChap == "") {alert("Choose a Chapter from the dropdown.")}
-//     else{
-//     if (sectionName!= ""){
-  
-//     console.log(currChap);
-//     sectArray[currChap].push(sectionName);
-//     console.log(sectArray);
-//     console.log(`top`);
-//     fillTags();
-//     clearS();}
-//     else{
-//       alert("Please give your tag a name. Labels are a great way to organize your notes and can help you review later!");
-//     }}
-//   }
-  
-  
-  
-//   function addChapter(){
-    
-//     const addBtn = document.getElementById("chapters-btn");
-  
-//     const sectionTopper = document.getElementById("section-topper");
-//     const sectionTop = document.createElement("div");
-//     sectionTop.className = "add-area";
-//     const titleArea = document.createElement("div");
-//     titleArea.className = "title-area";
-    
-  
-//     // const titleLabel = document.createElement("label");
-//     // titleLabel.setAttribute("for", "new-title");
-//     const titleInput = document.createElement("input");
-//     titleInput.id = "new-title";
-//     titleInput.setAttribute("placeholder", "New Chapter");
-//   titleArea.appendChild(titleInput);
-//     const startLabel = document.createElement("label");
-//     startLabel.setAttribute("for", "start-pg");
-//     startLabel.textContent = "Starts on Page: ";
-//     const startInput = document.createElement("input");
-//     startInput.id ="start-pg";
-//     startInput.setAttribute("type", "number"); 
-//     startInput.setAttribute("placeholder", "Starts on Page");
-  
-    
-//     sectionTop.appendChild(startLabel);
-//     sectionTop.appendChild(startInput);
-//   titleArea.appendChild(addBtn);
-//     sectionTop.appendChild(titleArea);
-  
-  
-//     sectionTopper.appendChild(sectionTop);
-  
-//   }
